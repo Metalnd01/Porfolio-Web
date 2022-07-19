1b0235fa246335cca53fc778367eb90341bc73f0
Metalnd01 <williamkurtmorales@gmail.com>
2022-07-19 14:36:14 -0300

    server port agregado

--- backEnd/src/main/java/com/porfolio/wkm/WkmApplication.java
+++ backEnd/src/main/java/com/porfolio/wkm/WkmApplication.java
@@ -37,7 +37,7 @@ public class WkmApplication {
             return new WebMvcConfigurer(){
                 @Override
                 public void addCorsMappings(CorsRegistry registry){
-                    registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("*");
+                    registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*");
                 }
             };
         }
